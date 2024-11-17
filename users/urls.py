f6bc5b3187d32265e94d3d7a1e2226dc34dbf8e7
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-11-18 01:16:52 +0300

    fixes permission

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ from users.views import UserCreateAPIView
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('register/', UserCreateAPIView.as_view(permission_classes=(AllowAny,)), name='register'),
-    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
