57a72851910fed1d2b52887192d1fc2bdf7f2363
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-11-27 02:21:00 +0300

    update csu

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -1,14 +1,20 @@
+import os
+
 from django.core.management import BaseCommand
+from dotenv import load_dotenv
 
 from users.models import User
+
+
+load_dotenv()
 
 
 class Command(BaseCommand):
     """Класс для кастомной команды создания суперпользователя"""
 
     def handle(self, *args, **options):
-        user = User.objects.create(email="admin@shop.ru")
-        user.set_password("admin")
+        user = User.objects.create(email=os.getenv("ADMIN_EMAIL"))
+        user.set_password(os.getenv("ADMIN_PASSWORD"))
         user.is_active = True
         user.is_staff = True
         user.is_superuser = True
